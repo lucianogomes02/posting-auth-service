acfbf1d69d4a1871f863f282a61b44d341d7a887
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-24 04:21:57 -0300

    refactor: change api definitions

--- application/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from src.routers import user, auth
-
-
-def create_api():
-    api = FastAPI()
-    api.include_router(user.router)
-    api.include_router(auth.router)
-    return api
